0ac21bac78ed67173d59475fb646336a7a72a71c
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-09-17 12:29:02 +0530

    Two Static inner Classes
    Two Static inner Classes and both classes have static method

---
+++ StaticInnerClasses.java
@@ -0,0 +1,22 @@
+class A{
+    static class B{
+        static void show1(){
+            System.out.println("static method inside static inner class B");
+        }
+    }
+    static class C{
+        static void show2(){
+            System.out.println("static method inside static inner class C");
+        }
+    }
+}
+public class StaticInnerClasses {
+    public static void main(String[] args) {
+        A.B.show1();
+        A.C.show2();
+    }
+}
+/*5)
+class A 
+	static class B static method 
+	static class C static method */
