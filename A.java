f41dc940f25c37219fba1cda41753895199d7ac8
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-09-19 11:35:20 +0530

    Basic Aggreation Example

---
+++ A.java
@@ -0,0 +1,11 @@
+class A {
+    public void show(){
+        System.out.println("Aggreation Example");
+    }    
+}
+// file A 
+
+// no return and no argument 
+
+// file Main 
+// aggregation
---
+++ Aggregation.java
@@ -0,0 +1,12 @@
+public class Aggregation{
+    public static void main(String[] args) {
+        A a = new A();
+        a.show();
+    }
+}
+/*file A 
+
+no return and no argument 
+
+file Main 
+aggregation*/
