c4c7639166a8304f47713b9ce0e77c224298b659
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-09-19 11:39:00 +0530

    Constructor With Aggregation

---
+++ A3.java
@@ -0,0 +1,6 @@
+class A3 {
+    A3(int a){
+        System.out.println("A3 class Parameterized constructor: "+a);
+    }
+}
+//file A constructor parameterised constructor
---
+++ Aggreation4.java
@@ -0,0 +1,12 @@
+public class Aggreation4 {
+    Aggreation4(){
+        System.out.println("Main class Aggreation4 default construtor");
+    }
+    public static void main(String[] args){
+        new A3(80);
+        new Aggreation4();
+    }  
+}
+/*file A constructor parameterised constructor 
+
+file Main constructor*/
