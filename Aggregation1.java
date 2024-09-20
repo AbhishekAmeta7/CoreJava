4822fd30629657169e5e3585393708c4c24d004b
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-09-20 18:13:23 +0530

    Basic Example Of Abstraction in Aggregation

---
+++ Aggregation1.java
@@ -0,0 +1,10 @@
+public class Aggregation1{
+    public static void main(String[] args) {
+        ExtendsAbs ea = new ExtendsAbs();
+        ea.show();
+        ea.show1(7);
+        ea.show2();
+        System.out.println(ea.show3("Hello!..Good Morning."));
+        ea.show4();
+    }
+}
