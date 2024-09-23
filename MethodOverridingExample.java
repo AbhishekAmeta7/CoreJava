399699d5d8d19749246dda2aed797554ed92a54d
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-09-24 00:00:00 +0530

    Method Overrriding

---
+++ MethodOverridingExample.java
@@ -0,0 +1,8 @@
+public class MethodOverridingExample {
+    public static void main(String[] args) {
+        
+    }
+}
+// 9.Design a `Vehicle` class with an abstract method `move()`. Extend it into subclasses 
+// `Car`, `Bike`, and `Boat`. Override the `move()` method to define how each vehicle moves. 
+// Demonstrate dynamic method dispatch using these classes.
