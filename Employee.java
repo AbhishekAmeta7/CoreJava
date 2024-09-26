8a47f2239be07822e2d513c4f5058480385726e5
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-09-26 23:48:53 +0530

    This class Belongs to ReferenceVariableAggregation class

---
+++ Employee.java
@@ -0,0 +1,36 @@
+class Employee {
+    int id;
+    String name;
+    String companyName;
+    String department;
+    long contact;
+    String address;
+    String email;
+    String password;
+    int salary;
+    Employee(int id,String name,String companyName,String department,long contact,String address,String email,String password,int salary){
+        this.id=id;
+        this.name=name;
+        this.companyName=companyName;
+        this.department=department;
+        this.contact=contact;
+        this.address=address;
+        this.email=email;
+        this.password=password;
+        this.salary=salary;
+    }
+}
+// file A 
+// employe 
+// id
+// name
+// companyname
+// department
+// contact
+// address 
+// email
+// password 
+// salary 
+// file B 
+// aggregation and class data type variable used .
+// work as data type variable
