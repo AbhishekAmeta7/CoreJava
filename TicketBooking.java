59851d544aba91fb7f93a4056f3b541d00bb0c4d
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-09-27 17:49:02 +0530

    This is belongs to ReferenceVariableAggregation2

---
+++ TicketBooking.java
@@ -0,0 +1,27 @@
+class TicketBooking {
+    String name;
+    String address;
+    long contact;
+    String gender;
+    int noOfTicket;
+    String movieName;
+    TicketBooking(String name,String address , long contact,String gender,int noOfTicket,String movieName){
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+        this.gender = gender;
+        this.noOfTicket = noOfTicket;
+        this.movieName = movieName;
+    }
+}
+// file A
+// ticket booking class 
+// name
+// address
+// contact
+// gender
+// total_number
+// movie_name
+
+// file B
+// work as data type variable
