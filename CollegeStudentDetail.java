7b79be933eda426d68589c39515d56a1924b6890
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-09-27 17:50:37 +0530

    This is belongs to ReferenceVariableAggregation3 Aggregation

---
+++ CollegeStudentDetail.java
@@ -0,0 +1,20 @@
+class CollegeStudentDetail {
+    String name;
+    String fname;
+    String mname;
+    String graduation;
+    String address;
+    long contact;
+    String groupName;
+    int sem;
+    CollegeStudentDetail(String name,String fname,String mname,String graduation,String address,long contact,String groupName,int sem){
+        this.name = name;
+        this.fname = fname;
+        this.mname = mname;
+        this.graduation = graduation;
+        this.address= address;
+        this.contact=contact;
+        this.groupName = groupName;
+        this.sem = sem;
+    }
+}
