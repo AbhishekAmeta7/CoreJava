0dd1ea4518ddae02a03f23e791383b09fd5b0470
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-09-27 17:46:31 +0530

    This Class Belongs to ReferenceVariableAggregation1

---
+++ Book.java
@@ -0,0 +1,34 @@
+public class Book {
+    String title;
+    String author;
+    String type;
+    String name;
+    int page;
+    String content;
+    int price;
+    int date;
+    Book(String title,String author,String type,String name,int page,String content,int price,int date){
+        this.title = title;
+        this.author=author;
+        this.type = type;
+        this.name=name;
+        this.page=page;
+        this.content=content;
+        this.price=price;
+        this.date=date;
+    }
+}
+// file A
+// book class 
+
+// title 
+// author 
+// type
+// name
+// page
+// content
+// price 
+// date
+
+// file B
+// work as data type variable 
