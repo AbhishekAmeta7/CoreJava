cbc2cb80a0569cbc2c86f80eb471acd89666ced7
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-09-30 23:50:35 +0530

    Sum of Odd Element of an array

---
+++ SumOddElementArray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumOddElementArray {
+    public static void printSumOddArray(int[]a){
+        int sum=0;
+        for(int i = 0;i<a.length;i++){
+            if(a[i]%2!=0){
+                sum+=a[i];
+            }
+        }
+        System.out.print("Sum Of Odd Element: "+sum);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size Of Array: ");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        printSumOddArray(a);
+    }
+}
