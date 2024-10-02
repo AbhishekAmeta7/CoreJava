14695fee8a7f57c31c980564fd4b521a2017cf58
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-10-02 23:27:37 +0530

    Print Even Index Element in An Array in java

---
+++ PrintEvenIndexElement.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PrintEvenIndexElement {
+    public static void printArray(int a[]){
+        for (int i = 0; i < a.length; i++) {
+            if(i%2==0){
+                System.out.print(a[i]+ " ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size of Array: ");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        printArray(a);
+    }
+} 
