28ff2325f1f149e3f2e779128bc291902465e558
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-10-05 23:30:08 +0530

    Count the element that is greater than 12 in array

---
+++ Array_Assignment2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Array_Assignment2 {
+    public static int printArray(int a[]){
+        int count = 0;
+        for (int i : a) {
+            if(i>12){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size Of Array: ");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println(printArray(a));
+    }
+}
