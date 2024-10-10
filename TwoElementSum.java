36410b6f923ac793c4cf797272c45e2524c303c6
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-10-10 11:53:28 +0530

    Two Elements sum and equal to target Element in array

---
+++ TwoElementSum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class TwoElementSum{
+    public static void printArray(int a[],int target){
+        for(int i = 0; i < a.length; i++){
+            for(int j = i+1; j < a.length; j++){
+                if(a[i]+a[j] == target){
+                    System.out.println(a[i]+" "+a[j]);
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size Of Array: ");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+        System.out.print("Enter Target Element: ");
+        int target = sc.nextInt();
+        printArray(a,target);
+    }
+}
