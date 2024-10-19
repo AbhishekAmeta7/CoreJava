af4b0c02f0a9888191339a93125cf78c36599177
Abhishek Kumar Vyas <abhishekameta55@gmail.com>
2024-10-19 23:25:07 +0530

    Find Maximum Element From 2-D Array

---
+++ Maximum2D.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Maximum2D {
+    public static void findMax(int a[][]){
+        int x = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if(a[i][j]>x){
+                    x = a[i][j];
+                }
+            }
+        }
+        System.out.println("Maximum Element: "+x);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter No. Of Row & Column: ");
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a[][] = new int[n][m];
+        for (int i = 0; i <n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        findMax(a);
+    }
+}
